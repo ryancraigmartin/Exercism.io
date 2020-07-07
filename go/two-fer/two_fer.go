905b75604f37440c84edd61dc37f4175827b06e4
Ryan Craig Martin <ryan@ryanmartin.io>
2020-07-07 18:37:00 -0400

    refactor old Go solution

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -1,12 +1,9 @@
 package twofer
 
-import "fmt"
-
 // ShareWith accepts a name and returns a string
 func ShareWith(name string) string {
 	if name == "" {
-		return "One for you, one for me."
-	} else {
-		return fmt.Sprintf("One for %s, one for me.", name)
+		name = "you"
 	}
+	return "One for " + name + ", one for me."
 }
